2125c41219f1208715c04f263c60ef3b3fd4762c
gogojames <y.james518@gmail.com>
2013-07-07 17:33:06 +0800

    Signed-off-by: gogojames <y.james518@gmail.com>

--- InvoiceConversion/Data/Company.cs
+++ InvoiceConversion/Data/Company.cs
@@ -8,6 +8,18 @@ namespace InvoiceConversion.Data
   public  class Company
     {
         private string company_name;
+
+        public string Company_name
+        {
+            get { return company_name; }
+            set { company_name = value; }
+        }
         private int company_id;
+
+        public int Company_id
+        {
+            get { return company_id; }
+            set { company_id = value; }
+        }
     }
 }
