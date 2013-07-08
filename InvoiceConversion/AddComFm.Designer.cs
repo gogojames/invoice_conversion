4d2749a1eb5bee118f921ee5451aaa8bd511bb7d
gogojames <y.james518@gmail.com>
2013-07-08 21:19:47 +0800

    rename

---
+++ InvoiceConversion/AddComFm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace InvoiceConversion
+{
+    partial class AddComFm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "標題名稱";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(25, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(334, 208);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AddComFm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 284);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "AddComFm";
+            this.Text = "自定義發票標題";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ InvoiceConversion/Data/Invoice_master.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceConversion.Data
+{
+    [Serializable]
+   public class Invoice_master:Common.BusinessObjectBase
+    {
+        private string client_id;
+
+        public string Client_id
+        {
+            get { return client_id; }
+            set {
+                if (client_id != value)
+                {
+                    if (this.InEdit)
+                    {
+                        SaveState("Client_id", client_id);
+                        SaveData("Client_id", value);
+                    }
+                    client_id = value;
+                    OnPropertyChanged("Client_id");
+                }
+                 }
+        }
+        private string client_name;
+
+        public string Client_name
+        {
+            get { return client_name; }
+            set {
+                if (client_name != value)
+                {
+                    if (this.InEdit)
+                    {
+                        SaveState("Client_name", client_name);
+                        SaveData("Client_name", value);
+                    }
+                    client_name = value;
+                    OnPropertyChanged("Client_name");
+                }
+            }
+        }
+        private string invoice_nmber;
+
+        public string Invoice_nmber
+        {
+            get { return invoice_nmber; }
+            set {
+                if (invoice_nmber != value)
+                {
+                    if (this.InEdit)
+                    {
+                        SaveState("Invoice_nmber", invoice_nmber);
+                        SaveData("Invoice_nmber", value);
+                    }
+
+                    invoice_nmber = value;
+                    OnPropertyChanged("Invoice_nmber");
+                }
+            }
+        }
+        private int invoice_title_id;
+
+        public int Invoice_title_id
+        {
+            get { return invoice_title_id; }
+            set {
+                if (invoice_title_id != value)
+                {
+                    if (InEdit)
+                    {
+                        SaveState("Invoice_title_id", invoice_title_id);
+                        SaveData("Invoice_title_id", value);
+                    }
+                    invoice_title_id = value;
+                    OnPropertyChanged("Invoice_title_id");
+                }
+                }
+        }
+        private string ainvoice_id;
+
+        public string Ainvoice_id
+        {
+            get { return ainvoice_id; }
+            set {
+                if (ainvoice_id != value)
+                {
+                    if (InEdit)
+                    {
+                        SaveState("Ainvoice_id", ainvoice_id);
+                        SaveData("Ainvoice_id", value);
+                    }
+                    ainvoice_id = value;
+                    OnPropertyChanged("Ainvoice_id");
+                }
+                }
+        }
+        private DateTime invoice_date;
+
+        public DateTime Invoice_date
+        {
+            get { return invoice_date; }
+            set
+            {
+                if (invoice_date != value)
+                {
+                    if (InEdit)
+                    {
+                        SaveState("Invoice_date", invoice_date);
+                        SaveData("Invoice_date", value);
+                    }
+                    invoice_date = value;
+                    OnPropertyChanged("Invoice_date");
+                }
+            }
+        }
+    }
+}
